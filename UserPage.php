15a616a398070c6b9b314e1a0f7ed85612564f14
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-06-20 11:26:52 -0600

    RM
    Test Google maps.

--- UserPage.php
+++ UserPage.php
@@ -22,5 +22,15 @@
 			<p>Logged in as: <?= $fgmembersite->UserFullName() ?></p>
 			<p><a href='login-home.php'>Home</a></p>
 		</div>
+		
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<div style="width:450px;color:#555"><div style="overflow:hidden;height:300px;width:450px;">
+		<div id="gmap_canvas" style="height:300px;width:450px;"></div>
+		<style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
+		</div><script type="text/javascript">
+		 function init_map(){var myOptions = {zoom:15,center:new google.maps.LatLng(31.75710249999999,-106.49350859999998),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(31.75710249999999, -106.49350859999998)});infowindow = new google.maps.InfoWindow({content:"<b>1914 Lounge</b><br/>115 durango<br/> el paso" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script><div style="text-align:right;font-family:arial;font-size:10px;">Google Maps Generator by <a style="text-decoration:none;color:#555;" href="http://www.map-embed.com">www.map-embed.com</a>
+		 </div>
+		 </div>
+		
 	</body>
 </html>
