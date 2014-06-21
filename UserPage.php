02e63b22ffbabff6fb4bcf6f18e395b71bd7da5a
ecorra37 <ecorra37@gmail.com>
2014-06-20 19:46:46 -0600

    EC

--- UserPage.php
+++ UserPage.php
@@ -16,15 +16,15 @@
 		<link rel="STYLESHEET" type="text/css" href="css/fg_membersite.css">
 	</head>
 	
-	<body>
-		<div id='fg_membersite_content'>
+	<body >
+	<div id="main_container">
+		<div id='middle_box'>
 			<h2>This is an Access Controlled Page</h2>
 			This page can be accessed after logging in only. To make more access controlled pages, 
 			copy paste the code between &lt;?php and ?&gt; to the page and name the page to be php.
 			<p>Logged in as: <?= $fgmembersite->UserFullName() ?></p>
 			<p>Logged in as: <?= $fgmembersite->UserName() ?></p>
 			<p><a href='login-home.php'>Home</a></p>
-		</div>
 		
 		<iframe
 			width="600"
@@ -33,5 +33,83 @@
 			src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB0uLEbR6K9fehSmaCyR4-NdWmIUaYevjY
 			&q=<?= $street?>">
 		</iframe>
+		<br/><br/><br/>
+		<br/><br/><br/>
+			<div class="middle_box_content">
+				<div align = "center">
+					<html lang="en">
+						<head>
+							<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+							<script src="//code.jquery.com/jquery-1.9.1.js"></script>
+							<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+							<link rel="stylesheet" href="/resources/demos/style.css">
+							<style>
+								#accordion-resizer {
+									padding: 10px;
+									width: 350px;
+									height: 200px;
+								}
+							</style>
+							<script>
+								$(function() {
+									$( "#accordion" ).accordion({
+										heightStyle: "fill"
+									});
+								});
+								$(function() {
+									$( "#accordion-resizer" ).resizable({
+										minHeight: 100,
+										minWidth: 200,
+										resize: function() {
+											$( "#accordion" ).accordion( "refresh" );
+										}
+									});
+								});
+							</script>
+						</head>
+						<body>
+							<div id="accordion">
+								<h3>1D Abstractions</h3>
+								<div>
+									<p>
+										<table>
+											<tr>
+											<td>
+												<font size="3" color="black"><b>Timeline</b></font>
+												<?php $src = "http://www.stanford.edu/group/spatialhistory/media/images/publication/MSantosAPeers_graphs-05.svg"; ?>
+												<a href='./SetQueryParameters.php?type=1D_Timeline&image=<?php echo $src ?>&desc=Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'><img src="http://www.stanford.edu/group/spatialhistory/media/images/publication/MSantosAPeers_graphs-05.svg" width="250" height="250">
+											</td>
+											<td>
+												Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+											</td>
+											</tr>
+										</table>
+									</p>
+								</div>
+								<h3>2D Abstractions</h3>
+								<div>
+									<p>
+										<table>
+											<tr>
+												<td>
+													<font size="3" color="black"><b>Contour Map</b></font><br>
+													<?php $src = "https://nanohub.org/infrastructure/rappture/raw-attachment/wiki/rp_xml_ele_field/contour.jpg"; ?>
+													<a href='./SetQueryParameters.php?type=2D_ContourMap&image=<?php echo $src ?>&desc=Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'><img src="https://nanohub.org/infrastructure/rappture/raw-attachment/wiki/rp_xml_ele_field/contour.jpg"  width="250" height="250"></a>
+												</td>
+												<td>
+													Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+												</td>
+											</tr>
+										</table>
+									</p>
+								</div>
+							</div>
+						</body>
+					</html>
+				</div>
+			</div>
+		</div>
+		</div>
 	</body>
 </html>
+
