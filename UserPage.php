550793707ac0fd777036cbc2681434e68942db4c
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-06-23 17:22:59 -0600

    RM

--- UserPage.php
+++ UserPage.php
@@ -27,72 +27,7 @@
 			/* IE has layout problems when sorting  */
 			.group { zoom: 1 }
 		</style>
-<<<<<<< HEAD
-=======
 
-		<script>
-			$(function() {
-			$( "#accordion" ).accordion({
-			heightStyle: "fill"
-			});
-			});
-			$(function() {
-			$( "#accordion-resizer" ).resizable({
-			minHeight: 100,
-			minWidth: 200,
-			resize: function() {
-			$( "#accordion" ).accordion( "refresh" );
-			}
-			});
-			});
-		
-			//-----------------------------------------------------------+
-			// addBout() - Add a single node to the accordian
-			// TODO: add JSON object as input parameter and build the
-			//       bout with that data,
-			//-----------------------------------------------------------+
-			function addBout(){
-				var bout =
-					"<h3>1D Abstractions</h3>"+
-						"<div>"+
-							"<p>"+
-								"<table>"+
-									"<tr>"+
-										"<td>"+
-											"hello"+
-										"</td>"+
-										"<td>"+
-											"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."+
-										"</td>"+
-									"</tr>"+
-								"</table>"+
-							"</p>"+
-						"</div>";
-				return bout;
-			}
-			 
-			$(function(){
-				$( "#accordion" )
-						.accordion({
-							header: "> .group > .handle",
-							heightStyle: "content"
-						})
-						.sortable({
-							axis: "y",
-							handle: ".handle",
-							stop: function( event, ui ) {
-								// IE doesn't register the blur event when sorting
-								// so trigger focusout handler to remove .ui-state-focus
-								ui.item.children(".handle").triggerHandler("focusout");
-							}
-						});
-						
-				for (var i = 0; i < 8; i++) {
-					$("#accordion").append(addBout()).accordion("refresh");
-				}
-			});
-		</script>
->>>>>>> origin/master
 	</head>
 	
 	<body onLoad="showEvents('ecorral2');">
@@ -114,10 +49,4 @@
 	</body>
 </html>
 
-<!--
-IDEA:
-	When the user clicks on the 'see my events' link,
-	The page should be loaded with the events.
-	Automatically the page should query the users events based on the username.
-	
--->
+
