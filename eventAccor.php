65e99a86c6d640ae5a9351f72203d0cf9d4b84a3
ecorra37 <ecorra37@gmail.com>
2014-06-23 21:02:59 -0600

    EC

---
+++ eventAccor.php
@@ -0,0 +1,71 @@
+<?PHP
+	require_once("./include/membersite_config.php");
+	if(!$fgmembersite->CheckLogin()){
+		$fgmembersite->RedirectToURL("login.php");
+		exit;
+	}
+	
+	$street = "vista+del+sol,el+paso+tx";
+	$usrname = $fgmembersite->UsrName();
+	
+	
+	$con = mysqli_connect('localhost', 'root', 'password', 'EventAdvisor');
+	
+	if (!$con) {
+		die('Could not connect: ' . mysqli_error($con));
+	}
+
+	mysqli_select_db($con, "EventAdvisor");
+	
+	$sql = "SELECT Evename, Eaddress, Ecity, Estate, Ezip FROM Events WHERE UuserName = '" . $usrname . "'";
+	
+	$result = mysqli_query($con, $sql);
+?>
+
+<html>
+	<head>
+		<link rel="STYLESHEET" type="text/css" href="css/fg_membersite.css">
+		<link href="css/accordion.css" rel="stylesheet" type="text/css" />
+		
+		
+	</head>
+	
+	<body >
+		<div id="main_container">
+			<div id='middle_box'>
+				<!--displays the full name and username of the user-->
+				<p>Logged in as: <?= $fgmembersite->UserFullName() ?></p>
+				<p>Logged in as: <?= $usrname?></p>
+		
+				<?PHP
+					$i = 0;
+					while($row = mysqli_fetch_array($result)){ 
+					
+					//if the street name contains two or more words, the map will not recognize the street.
+					$street = $row['Eaddress'] . ", " . $row['Ecity'] . ", " . $row['Estate'] . " " . $row['Ezip'];
+					
+					echo $street;
+				?>
+					<div class="accordion vertical">
+						<ul>
+							<li>
+								<input type="radio" id="radio-<?= $i?>" name="radio-accordion" checked="checked" />
+								<label for="radio-<?= $i?>">Event <?= $i?></label>
+								<div class="content">
+									<h3>hello test</h3>
+									<p><?= $row['Evename'] ?></p>
+									<p><iframe
+										width="600"
+										height="450"
+										frameborder="0" style="border:0"
+										src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB0uLEbR6K9fehSmaCyR4-NdWmIUaYevjY&q=<?= $street?>">
+									</iframe></p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				<?PHP $i++; } ?>
+			</div>
+		</div>
+	</body>
+</html>
