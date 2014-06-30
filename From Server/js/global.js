59a06e80f91ab65f7eef52bcd9b10a15ebeefc34
flake79936 <ROBERT.MADRID14@GMAIL.COM>
2014-06-30 11:29:53 -0600

    RM

--- From Server/js/global.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document//clear text box on click
-function clearText(field){
-
-if (field.defaultValue == field.value) field.value = '';
-else if (field.value == '') field.value = field.defaultValue;
-
-}
